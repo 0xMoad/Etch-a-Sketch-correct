ab291eae74bca91ca12f8e9ccc29649755f622d3
Moad <moboa005@osloskolen.no>
2022-07-15 23:03:03 +0200

    Last game change, up and running

--- app.js
+++ app.js
@@ -3,29 +3,32 @@ let button = document.getElementById("button");
 let reset = document.getElementById("reset");
 let color  = document.getElementById("color");
 let bgColor = "red"
+let eraser = document.getElementById("eraser");
 
-
+//Color changer
 color.addEventListener("change", (e) => (bgColor = e.target.value));
+
 
 
 const grid = () => {
     button.addEventListener("click",() =>{
+        // Getting userinput for the gridArea
         let inputValue = prompt("How many squares would you want")
         container.style.gridTemplateColumns = `repeat(${inputValue}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${inputValue}, 1fr)`;
         button.classList.add("button");  
+         // Creating the grid
         for(let i = 0; i < inputValue * inputValue; i++) {
             let cell = document.createElement("div");
             cell.classList.add("cell");
             container.appendChild(cell); 
-            
              button.disabled = true;
              button.innerHTML = "RESET TO SIZE AGAIN"
             
  } })         
     } 
 
-
+ // reset by emptying the container
 reset.addEventListener("click", function () {
     let cell = document.getElementsByClassName("grid")
     container.innerHTML = ""
@@ -37,6 +40,9 @@ reset.addEventListener("click", function () {
 // Execute 'start' function on mousedown anywhere in document.
 document.addEventListener("mousedown", start);
 
+document.addEventListener("keydown", erase)
+document.addEventListener("keyup", stopErase)
+
 // Execute 'stop' function on mouseup anywhere in document.
 document.addEventListener("mouseup", stop);
 
@@ -47,8 +53,8 @@ function start(event) {
     divs.forEach((div) => {
         div.addEventListener("mousemove", function(e){
             this.style.backgroundColor = `${bgColor}`;;
-            this.style.border = "1px solid black"
-            console.log("start works")
+            this.style.border = "1px solid gray"
+            
         })
     })
 }
@@ -61,6 +67,29 @@ function stop() {
         div.replaceWith(div.cloneNode(true));
     })
 }
+
+
+ function erase(event) {
+     let divs = document.querySelectorAll(".cell")
+     divs.forEach((div) => {
+         div.addEventListener("mousemove", function(e){
+             this.style.backgroundColor = "white";
+             this.style.border = "1px solid gray"
+            
+         })
+     })
+ }
+
+ function stopErase() {
+    let divs = document.querySelectorAll(".cell")
+    divs.forEach((div) => {
+        div.replaceWith(div.cloneNode(true));
+    })
+}
+
+
+
+
 
 
 
